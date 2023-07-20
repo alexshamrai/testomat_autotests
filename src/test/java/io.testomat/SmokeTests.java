8cf99ff339754b329edd4b7ddc41637be88fe30c
Alex Shamrai <gibsonshamray@gmail.com>
2023-07-20 21:54:50 +0300

    Add Base test and soft asserts

--- src/test/java/io.testomat/SmokeTests.java
+++ src/test/java/io.testomat/SmokeTests.java
@@ -1,27 +1,23 @@
 package io.testomat;
 
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+
 import com.github.javafaker.Faker;
 import io.testomat.ui.ProjectsPage;
 import io.testomat.ui.SignInPage;
 import io.testomat.ui.data.BaseProjectInfo;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.testomat.ui.Preloaders.disappearsMainPreloader;
 
-public class SmokeTests {
+public class SmokeTests extends BaseTest {
 
     Faker faker = new Faker();
     ProjectsPage projectsPage = new ProjectsPage();
-
-    static {
-        Configuration.baseUrl = "https://uat.testomat.io/";
-        Configuration.pollingInterval = 100;
-    }
 
     @BeforeEach
     void openLoginForm() {
@@ -54,5 +50,33 @@ public class SmokeTests {
         projectsPage
             .assertThat(expectedProjectTile)
             .hasCorrectInfo();
+    }
+
+    @Test
+    @DisplayName("Create test case test SOFT")
+    void createTestCaseTest() {
+        String targetProjectName = faker.rockBand().name();
+
+        projectsPage
+            .isLoaded()
+            .createTestProject(targetProjectName);
+
+        disappearsMainPreloader();
+
+        var expectedProjectTile = BaseProjectInfo.builder()
+                                                 .name(targetProjectName)
+                                                 .count("0")
+                                                 .label(BaseProjectInfo.ProjectType.CLASSICAL)
+                                                 .build();
+        open("/");
+
+        projectsPage
+            .assertThat(expectedProjectTile)
+            .hasCorrectInfo();
+    }
+
+    @AfterEach
+    void tearDown() {
+        Selenide.closeWebDriver();
     }
 }
