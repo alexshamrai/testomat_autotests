6c3e0cccb08bda9aceaeec3bc1686f3dcfcc44f1
Alex Shamrai <gibsonshamray@gmail.com>
2023-08-30 23:37:11 +0300

    Add CompaniesTest

---
+++ src/main/java/io/testomat/ui/asserts/CompaniesPageAsserts.java
@@ -0,0 +1,20 @@
+package io.testomat.ui.asserts;
+
+import lombok.AllArgsConstructor;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+@AllArgsConstructor
+public class CompaniesPageAsserts {
+
+    public CompaniesPageAsserts companyIsDeleted(String companyName) {
+        $(".common-flash-success").shouldHave(text("Company deleted successfully")
+                                                  .because("Company deleted successfully label is present"));
+        $$("td").filterBy(text(companyName)).shouldHave(size(0)
+                                                            .because(companyName + "should be deleted"));
+        return this;
+    }
+}
