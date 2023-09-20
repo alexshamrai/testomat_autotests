d49bc8d493136589893f524f9f256a2c02612c90
Alex Shamrai <gibsonshamray@gmail.com>
2023-09-20 22:48:17 +0300

    Add ExtentReport to api tests

--- src/test/java/io/testomat/api/BaseTest.java
+++ src/test/java/io/testomat/api/BaseTest.java
@@ -4,14 +4,16 @@ import com.github.javafaker.Faker;
 import io.testomat.api.login.CredentialsLoader;
 import io.testomat.api.login.LoginController;
 import io.testomat.api.login.model.Credentials;
+import io.testomat.report.ExtentReportListener;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 @Tag("api")
+@ExtendWith(ExtentReportListener.class)
 public abstract class BaseTest {
 
     static final String PROJECT_ID = "baseproject";
-
     static Faker faker = new Faker();
 
     String authToken;
