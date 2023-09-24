0445589407c51e27c640bbc60b605dfdb040fde8
Alex Shamrai <gibsonshamray@gmail.com>
2023-09-24 15:46:58 +0300

    Add console logs to ExtentReport

--- src/test/java/io/testomat/report/ExtentReportListener.java
+++ src/test/java/io/testomat/report/ExtentReportListener.java
@@ -1,5 +1,11 @@
 package io.testomat.report;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
@@ -14,6 +20,9 @@ public class ExtentReportListener implements BeforeAllCallback, BeforeTestExecut
 
     private static ExtentReports reports;
     private static ExtentTest test;
+    private static final ThreadLocal<ByteArrayOutputStream> outContent = new ThreadLocal<>();
+    private static final PrintStream originalOut = System.out;
+    private static final PrintStream originalErr = System.err;
 
     @Override
     public void beforeAll(ExtensionContext context) {
@@ -25,16 +34,24 @@ public class ExtentReportListener implements BeforeAllCallback, BeforeTestExecut
 
     @Override
     public void beforeTestExecution(ExtensionContext context) {
+        var baos = new ByteArrayOutputStream();
+        outContent.set(baos);
+        System.setOut(new PrintStream(new LoggingOutputStream(baos, originalOut)));
+        System.setErr(new PrintStream(new LoggingOutputStream(baos, originalErr)));
+
         test = reports.createTest(context.getDisplayName());
         test.log(Status.INFO, context.getDisplayName() + " - started");
     }
 
     @Override
     public void afterTestExecution(ExtensionContext context) {
-        if (context.getExecutionException().isEmpty()) {
-            test.pass(context.getDisplayName() + " - passed");
+        addConsoleLogs();
+
+        if (context.getExecutionException().isPresent()) {
+            var stackTrace = getStackTrace(context);
+            test.fail("Stack Trace: " + stackTrace);
         } else {
-            test.fail(context.getExecutionException().get().getLocalizedMessage());
+            test.pass(context.getDisplayName() + " - passed");
         }
     }
 
@@ -42,4 +59,42 @@ public class ExtentReportListener implements BeforeAllCallback, BeforeTestExecut
     public void afterAll(ExtensionContext context) {
         reports.flush();
     }
+
+    private static String getStackTrace(ExtensionContext context) {
+        var throwable = context.getExecutionException().get();
+        test.fail(throwable.getLocalizedMessage());
+
+        var stringWriter = new StringWriter();
+        var printWriter = new PrintWriter(stringWriter);
+        throwable.printStackTrace(printWriter);
+        return stringWriter.toString();
+    }
+
+    private static void addConsoleLogs() {
+        var content = outContent.get();
+        if (content != null && content.size() > 0) {
+            test.info("Console Logs: " + content);
+        }
+
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+        outContent.remove();
+    }
+
+    static class LoggingOutputStream extends OutputStream {
+        private final ByteArrayOutputStream baos;
+        private final PrintStream original;
+
+        public LoggingOutputStream(ByteArrayOutputStream baos, PrintStream original) {
+            this.baos = baos;
+            this.original = original;
+        }
+
+        @Override
+        public void write(int b) {
+            baos.write(b);
+            original.write(b);
+        }
+    }
+
 }
